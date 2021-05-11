12abac365a82f8f1507e97b55ebb9b5e6563e0c0
donkey_zwx <zengwanxin@bt-iot.com>
2021-05-11 10:03:37 +0800

    Leetcode：第20题：有效的括号

---
+++ Leetcode/src/main/java/com/donkey/leetcode/No20.java
@@ -0,0 +1,47 @@
+package com.donkey.leetcode;
+
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+import java.util.Stack;
+
+/**
+ * @Author zwx
+ * @Date 2021/5/10 22:42
+ * @Description  20. 有效的括号
+ * @Modified By:
+ * @Version:1.0.0
+ */
+public class No20 {
+
+    /**
+     * 如果是 '{','[','(' 添加到stack中
+     * 如果不是 出栈 判断stack顶端 和 现在这个括号是否是成对的
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character top = stack.pop();
+                if (c == '}' && top != '{') {
+                    return false;
+                }
+                if (c == ']' && top != '[') {
+                    return false;
+                }
+                if (c == ')' && top != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
